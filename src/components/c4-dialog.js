0ca81f92d9230e24b37fa20728818c31dba9baa0
Maria Paula Herrero <mpaulaherrero@gmail.com>
2023-07-13 13:49:00 -0400

    nombre de constantes correctas

--- src/components/c4-dialog.js
+++ src/components/c4-dialog.js
@@ -1,9 +1,10 @@
 import { LitElement, html, css } from 'lit';
 
 export class C4Dialog extends LitElement {
-    #welcomeText = 'Si quieres jugar contra la IA seleciona una columna, si no, cambia la opción y empieza otro juego';
-    #selectColumnText = `Selecciona una columna`;
-    #fullColumnText = `La columna esta llena, intente con otra`;
+    WELCOME_TEXT = 'Si quieres jugar contra la IA seleciona una columna, si no, cambia la opción y empieza otro juego';
+    SELECT_COLUMN_TEXT = `Selecciona una columna`;
+    FULL_COLUMN_TEXT = `La columna esta llena, intente con otra`;
+    WINNER_TEXT = `Victoria para`;
 
     static styles = [
         css`
@@ -26,9 +27,9 @@ export class C4Dialog extends LitElement {
         super();
         window.addEventListener('write-text', (e) => this.write(e.detail.message));
         //window.addEventListener('write-welcome', () => this.write(this.#welcomeText));
-        window.addEventListener('write-select-column', () => this.write(this.#selectColumnText));
-        window.addEventListener('write-select-column_not_welcome', () => this.writeIfNotWelcome(this.#selectColumnText));
-        window.addEventListener('write-full-column', () => this.write(this.#fullColumnText));
+        window.addEventListener('write-select-column', () => this.write(this.SELECT_COLUMN_TEXT));
+        window.addEventListener('write-select-column_not_welcome', () => this.writeIfNotWelcome(this.SELECT_COLUMN_TEXT));
+        window.addEventListener('write-full-column', () => this.write(this.FULL_COLUMN_TEXT));
     }
 
     render() {
@@ -36,7 +37,7 @@ export class C4Dialog extends LitElement {
     }
 
     writeWelcome(){
-        this.write(this.#welcomeText);
+        this.write(this.WELCOME_TEXT);
     }
 
     write(text){
@@ -44,9 +45,17 @@ export class C4Dialog extends LitElement {
     }
 
     writeIfNotWelcome(text){
-        if(this.text !== this.#welcomeText){
+        if(this.text !== this.WELCOME_TEXT){
             this.write(text);
         }
+    }
+
+    writeWinner(element){
+        this.write(this.WINNER_TEXT + " " + element );
+        //const dialogElement = document.getElementById('dialog');
+        //dialogElement.innerHTML=text;
+        //dialogElement.append(element);
+        //dialogElement.parentNode.insertBefore(element,dialogElement.nextSibling);
     }
 }
 customElements.define('c4-dialog', C4Dialog);
